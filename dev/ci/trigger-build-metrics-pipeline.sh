707d2cfa22bfbd1661060267b07626e34cb1ce87
Noah Santschi-Cooney <noah@santschi-cooney.ch>
2024-03-26 18:13:27 +0000

    wip

---
+++ dev/ci/trigger-build-metrics-pipeline.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -eux pipefail
+
+cat > build-metrics-pipeline.yaml <<EOF
+steps:
+  - label: :eye: Build Metrics
+    trigger: devx-build-metrics
+    async: true
+    depends_on: "__main__::finalization"
+EOF
+
+buildkite-agent pipeline upload build-metrics-pipeline.yaml
