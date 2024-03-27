8fe88ccd413ba6dcbc96627e0d283574fe291a1e
Noah Santschi-Cooney <noah@santschi-cooney.ch>
2024-03-27 13:04:58 +0000

    wip

--- dev/ci/trigger-build-metrics-pipeline.sh
+++ dev/ci/trigger-build-metrics-pipeline.sh
@@ -8,6 +8,14 @@ steps:
     trigger: devx-build-metrics
     async: true
     depends_on: "__main__::finalization"
+    build:
+      message: "\${BUILDKITE_MESSAGE}"
+      commit: "\${BUILDKITE_COMMIT}"
+      branch: "\${BUILDKITE_BRANCH}"
+      env:
+        BUILDKITE_PULL_REQUEST: "\${BUILDKITE_PULL_REQUEST}"
+        BUILDKITE_PULL_REQUEST_BASE_BRANCH: "\${BUILDKITE_PULL_REQUEST_BASE_BRANCH}"
+        BUILDKITE_PULL_REQUEST_REPO: "\${BUILDKITE_PULL_REQUEST_REPO}"
 EOF
 
 buildkite-agent pipeline upload build-metrics-pipeline.yaml
